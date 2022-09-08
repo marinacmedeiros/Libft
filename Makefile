80cadf10473cc2adb61b177f30a06ee4462c2283
mamedeir <mamedeir@student.42sp.org.br>
2022-09-08 16:56:27 +0200

    save

---
+++ Makefile
@@ -0,0 +1,29 @@
+
+SRCS	= 
+
+OBJS	= ${SRCS: .c=.o}
+
+NAME	= 
+
+CC	= cc
+RM	= rm -f
+CFLAGS	= -Wall -Wextra -Werror
+
+.c.o:
+	${CC} ${CFLAGS} -c $< -o $< -o ${<:.c=.o}
+		
+${NAME}:	${OBJS}
+	${CC} -o ${NAME} ${OBJS}
+
+all:	${NAME}
+
+clean:
+	${RM} ${OBJS}
+
+fclean:	clean
+	${RM} ${NAME}
+
+re:	fclean all
+
+.PHONY:	all clean fclean re
+
--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,14 +6,39 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 20:46:44 by coder             #+#    #+#             */
-/*   Updated: 2022/09/07 20:56:32 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/08 14:56:32 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_isalnum(int i)
 {
-	if ((i >= '0' && i <= '0') || (i >= 'A' && i <= 'Z')
+	if ((i >= '0' && i <= '9') || (i >= 'A' && i <= 'Z')
 		|| (i >= 'a' && i <= 'z'))
 		return (1);
 	return (0);
+}
+
+int	main(void)
+{
+	int	a, b, c, d, f, g, h;
+	
+	a = '7';
+	b = '3';
+	c = 'X';
+	d = 'z';
+	//e = 'ç';
+	f = '*';
+	g = '54';
+	h = '-871';
+	
+	printf("%d\n", ft_isalnum(a));
+	printf("%d\n", ft_isalnum(b));
+	printf("%d\n", ft_isalnum(c));
+	printf("%d\n", ft_isalnum(d));
+	//printf("%d\n", ft_isalnum(e));
+	printf("%d\n", ft_isalnum(f));
+	printf("%d\n", ft_isalnum(g));
+	printf("%d\n", ft_isalnum(h));
 }
