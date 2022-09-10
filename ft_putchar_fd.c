efed30f2ed91882c55a5912d1a0e638f16870a41
mamedeir <mamedeir@student.42sp.org.br>
2022-09-10 17:34:32 +0200

    update

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,14 +6,13 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 23:18:35 by coder             #+#    #+#             */
-/*   Updated: 2022/09/10 16:22:54 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/10 17:26:30 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-void ft_putchar_fd(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
--- libft.h
+++ libft.h
@@ -6,24 +6,24 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 16:27:49 by coder             #+#    #+#             */
-/*   Updated: 2022/09/10 16:30:39 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/10 16:59:23 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 void	ft_putchar_fd(char c, int fd);
-size_t	ft_strlen(const charstr);
+size_t	ft_strlen(const char *str);
 
 #endif
