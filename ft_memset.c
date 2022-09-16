3d55597e3ff16ca1472e61e5ef43ee914810772e
mamedeir <mamedeir@student.42sp.org.br>
2022-09-16 16:30:54 +0200

    up

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,23 @@
 /*   By: mamedeir <mamedeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 14:43:55 by mamedeir          #+#    #+#             */
-/*   Updated: 2022/09/16 15:27:41 by mamedeir         ###   ########.fr       */
+/*   Updated: 2022/09/16 16:29:32 by mamedeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memset(void *s, int c, size_t len);
+void	*memset(void *str, int c, size_t len);
 {
-	
+	size_t	i;
+	char	cstr;
+
+	i = 0;
+	cstr = (char *) str;
+	while (i < len)
+	{
+		cstr[i] = c;
+		i++;
+	}
+	return (str);
 }
